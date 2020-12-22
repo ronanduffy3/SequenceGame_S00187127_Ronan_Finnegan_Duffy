8a883a63c2739836122c88bcdefb64f34a672554
Ronan Duffy <48518654+ronanduffy3@users.noreply.github.com>
2020-12-22 20:44:42 +0000

    Initial commit

---
+++ app/src/main/java/com/example/sequencegame_s00187127_ronan_finnegan_duffy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sequencegame_s00187127_ronan_finnegan_duffy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
